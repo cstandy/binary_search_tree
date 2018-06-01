8449d7f2db7164f7bf3a0afcffac38f5e1de7d7a
cstandy <cst.andy@gmail.com>
2018-06-01 22:51:09 +0800

    modify the header file

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -1,7 +1,6 @@
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
 
-#include <string>
 using namespace std;
 
 class BST
@@ -15,12 +14,17 @@ public:
     void insert(int);
     void remove(void);
 
+    /* basic characteristic of the object */
+    int height(void);
+    int node_number(void);
+
     /* traversal method */
     void print_graph(void);
     void print_inorder(void);
     void print_preorder(void);
     void print_postorder(void);
     void print_breadth_first(void);
+
 private:
     int key;
 };
