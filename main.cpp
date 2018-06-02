6690651defca1ae97db496f2c279300b10e6eca7
cstandy <cst.andy@gmail.com>
2018-06-02 23:03:50 +0800

    add main.cpp and change the implement file form .c to .cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * Author: cstandy, NCKUEE
+ *
+ * This main function aims to complete the user-interface of the binary
+ * search tree with the class BST (binary search tree). The fucntion about
+ * class BST is defined in binary_search_tree.h and implemented in 
+ * binary_search_tree.cpp.
+ * 
+ */
+
+#include <fstream>
+#include "binary_search_tree.h"
+
+using namespace std;
+
+int main()
+{
+
+
+   system("pause");
+   return 0;
+}
