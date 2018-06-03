9bcbf1bb062ec069b6fb21eff38b69aa03f8047c
cstandy <cst.andy@gmail.com>
2018-06-03 10:31:38 +0800

    add function prototype

---
+++ binary_search_tree.cpp
@@ -0,0 +1,56 @@
+#include "binary_search_tree.h"
+
+BST::BST(void)
+{
+
+}
+
+BST::~BST(void)
+{
+    // delete recursively
+}
+
+void BST::insert(int)
+{
+
+}
+
+void BST::remove(void)
+{
+
+}
+
+int BST::height(void)
+{
+
+}
+
+int BST::node_number(void)
+{
+
+}
+
+void BST::print_graph(void)
+{
+
+}
+
+void BST::print_inorder(void)
+{
+
+}
+
+void BST::print_preorder(void)
+{
+
+}
+
+void BST::print_postorder(void)
+{
+
+}
+
+void BST::print_breadth_first(void)
+{
+
+}
