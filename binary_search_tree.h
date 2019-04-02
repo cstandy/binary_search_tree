41317a6a9343c1bd6b3ef7366095d3d0e88081c4
cstandy <cst.andy@gmail.com>
2019-04-03 00:58:52 +0800

    Style: Align the code

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -8,60 +8,60 @@ using namespace std;
 class BST
 {
 public:
-    /* create binary tree with root node
+	/* create binary tree with root node
 	 * (construction without default key is invalid)
 	 */
-    BST(int);
-    ~BST();
+	BST(int);
+	~BST();
 
-    /* basic tree operations */
-    void insert(int);
-    bool search(int);
+	/* basic tree operations */
+	void insert(int);
+	bool search(int);
 
-    /* basic characteristic of the object */
-    int depth(void);
+	/* basic characteristic of the object */
+	int depth(void);
 
-    /* print traversal method */
-    void print_graph(void);
-    void print_inorder(void);
-    void print_preorder(void);
-    void print_postorder(void);
-    void print_breadth_first(void);
+	/* print traversal method */
+	void print_graph(void);
+	void print_inorder(void);
+	void print_preorder(void);
+	void print_postorder(void);
+	void print_breadth_first(void);
 
 private:
 	/* variable for single node */
-    int key;
+	int key;
 	BST* left_child;
 	BST* right_child;
 
 	/* variable for recursive search the tree,
 	 * treat current node as root.
 	 */
-    int height;
-    
+	int height;
+	
 	/* containers for recursive search in breadth-first method
 	 * 
-	 * queue<BST*> next_node       : queue for next node pointer
+	 * queue<BST*> next_node	   : queue for next node pointer
 	 * queue<int> BF_search_output : queue for the output-ordered key
 	 *
 	 */
-    queue<BST*> next_node;
-    queue<int> BF_search_output;
+	queue<BST*> next_node;
+	queue<int> BF_search_output;
 
 	/* recursive functions 
 	 *
-	 * void BF_search(queue<BST*>&, queue<int>&)                : breadth-firth method
-	 * void recursive_get_height(int&, int)                     : measure the depth
-     * void recursive_print_graph(short, short, COORD, HANDLE&) : print each node with position info
+	 * void BF_search(queue<BST*>&, queue<int>&)				: breadth-firth method
+	 * void recursive_get_height(int&, int)						: measure the depth
+	 * void recursive_print_graph(short, short, COORD, HANDLE&) : print each node with position info
 	 *
 	 */
-    void BF_search(queue<BST*>&, queue<int>&);
-    void recursive_get_height(int&, int);
-    void recursive_print_graph(short, short, COORD, HANDLE&);
+	void BF_search(queue<BST*>&, queue<int>&);
+	void recursive_get_height(int&, int);
+	void recursive_print_graph(short, short, COORD, HANDLE&);
 
-    /* variables for cursor moving provided by Windows.h */
-    HANDLE cursor_handle = GetStdHandle(STD_OUTPUT_HANDLE); // handle of Get stdout
-    COORD position;
+	/* variables for cursor moving provided by Windows.h */
+	HANDLE cursor_handle = GetStdHandle(STD_OUTPUT_HANDLE); // handle of Get stdout
+	COORD position;
 };
 
 #endif /* BINARY_SEARCH_TREE_H */
--- main.cpp
+++ main.cpp
@@ -21,74 +21,74 @@ using namespace std;
 
 int main()
 {
-    int input_data = 0;
-    int select = 0;
+	int input_data = 0;
+	int select = 0;
 
-    printf("please select to: 1) read input data, 2) randomly generate: ");
-    cin >> select;
+	printf("please select to: 1) read input data, 2) randomly generate: ");
+	cin >> select;
 
-    if (select == 1)
-    {
-        ifstream input_file(INPUT_FILE, ios::in);
+	if (select == 1)
+	{
+		ifstream input_file(INPUT_FILE, ios::in);
 
-        if (!input_file)
-        {
-            cerr << "Input file: " << INPUT_FILE << " open failed.\r\n";
-            exit(1);
-        }
+		if (!input_file)
+		{
+			cerr << "Input file: " << INPUT_FILE << " open failed.\r\n";
+			exit(1);
+		}
 
-        input_file >> input_data;
+		input_file >> input_data;
 		printf("\r\n");
-		printf("input data                   : %d", input_data);
+		printf("input data					 : %d", input_data);
 
-        BST tree(input_data);
+		BST tree(input_data);
 
 		while (input_file >> input_data)
 		{
 			tree.insert(input_data);
 			printf(" %d", input_data);
 		}
-            
+			
 		printf("\r\ntree depth: %d", tree.depth());
 
 		printf("\r\ngraph:\r\n");
 		tree.print_graph();
 
 		printf("\r\n\r\n");
-        printf("inorder   (left->root->right):");
-        tree.print_inorder();
+		printf("inorder   (left->root->right):");
+		tree.print_inorder();
 		printf("\r\n\r\n");
-        printf("preorder  (root->left->right):");
-        tree.print_preorder();
+		printf("preorder  (root->left->right):");
+		tree.print_preorder();
 		printf("\r\n\r\n");
-        printf("postorder (left->right->root):");
-        tree.print_postorder();
+		printf("postorder (left->right->root):");
+		tree.print_postorder();
 		printf("\r\n\r\n");
-        printf("breadth-first search         :");
-        tree.print_breadth_first();
-    }
-    else if (select == 2)
-    {
-        // may use different way to generate random numbers
+		printf("breadth-first search		 :");
+		tree.print_breadth_first();
+	}
+	else if (select == 2)
+	{
+		// may use different way to generate random numbers
 
 
-        printf("please enter the node number: ");
-        cin >> select;
+		printf("please enter the node number: ");
+		cin >> select;
 
 
-        srand(time(0));
+		srand(time(0));
 		input_data = rand() % 99 + 1;
 		printf("\r\n");
-		printf("input data                   : %d", input_data);
+		printf("input data					 : %d", input_data);
 
 		BST tree(input_data);
 
-        for (int i = 1; i < select; i++)
-        {
+		for (int i = 1; i < select; i++)
+		{
 			input_data = rand() % 99 + 1;
 			tree.insert(input_data);
 			printf(" %d", input_data);
-        }
+		}
 
 		printf("\r\ntree depth: %d", tree.depth());
 
@@ -105,12 +105,12 @@ int main()
 		printf("postorder (left->right->root):");
 		tree.print_postorder();
 		printf("\r\n\r\n");
-		printf("breadth-first search         :");
+		printf("breadth-first search		 :");
 		tree.print_breadth_first();
-    }
+	}
 
-    printf("\r\n");
+	printf("\r\n");
 
-    system("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
